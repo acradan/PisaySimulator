e65e81ed18a807556b13ea6174375f9c426a29a5
acradan <90022131+acradan@users.noreply.github.com>
2023-04-09 09:43:08 +0800

    Update and rename FXMLDocumentController.java to InputScreenController.java

--- src/pisaysimulator_adan_nora_palada/InputScreenController.java
+++ src/pisaysimulator_adan_nora_palada/InputScreenController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package projectmilestone1;
+package pisaysimulator_adan_nora_pala;
 
 import java.net.URL;
 import java.util.ResourceBundle;
